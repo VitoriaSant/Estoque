daa8f12ef2941d043b563fd4f9b899b94d4249b4
VitoriaSant <svitoria3465@gmail.com>
2026-04-01 21:51:33 -0300

    Cards

---
+++ front/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card
+        variant="outlined"
+        class="mx-auto"
+        color="primary"
+        :title="title"
+        :subtitle="subtitle"
+    >
+    <template v-slot:text>
+        VALOR 
+    </template>
+    </v-card>
+</template>
+<script setup>
+
+defineProps({
+    title: String,
+    subtitle: String
+})
+</script>
--- front/src/pages/comprasPendentes.vue
+++ front/src/pages/comprasPendentes.vue
@@ -1,69 +1,21 @@
 <template>
-    <div class="pa-4 text-center">
-        <v-dialog
-        v-model="dialog"
-        max-width="600"
-        >
-        <template v-slot:activator="{ props: activatorProps }">
-            <v-btn 
-                class="text-none font-weight-regular"
-                text="Edit Profile"
-                variant="tonal"
-                v-bind="activatorProps"
-                color="secondary" 
-                @click="ConsultarValores"> 
-                Consultar Valores </v-btn>
-        </template>
+    <v-row no-gutters>
+        <v-col class="pa-1">
+            <Card title="Pedidos Pendentes" subtitle="Total de Pedidos Pendentes"/>
+        </v-col>
+        <v-col class="pa-1">
+            <Card title="Valor Pendente" subtitle="Total de Valor Pendente"/>
+        </v-col>
+        <v-col class="pa-1">
+            <Card title="Pedidos em Atraso" subtitle="Total de Pedidos Pendentes em atraso"/>
+        </v-col>
+        <v-col class="pa-1">
+            <Card title="Valor Pendente em Atraso" subtitle="Total de Valor Pendente Em atraso"/>
+        </v-col>
+    </v-row>
 
-        <v-card>
-            <v-card-text>
-                <v-text-field
-                    :loading="loading"
-                    color="primary"
-                    append-inner-icon="mdi-magnify"
-                    density="compact"
-                    label="Consultar Valores"
-                    variant="solo"
-                    hide-details
-                    single-line
-                    @click:append-inner="onClick"
-                ></v-text-field>
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn
-                color="secondary"
-                text="Fechar"
-                variant="plain"
-                @click="dialog = false"
-            ></v-btn>
-
-            <v-btn
-                color="primary"
-                text="Confirmar"
-                variant="tonal"
-                @click="dialog = false"
-            ></v-btn>
-            </v-card-actions>
-        </v-card>
-        </v-dialog>
-    </div>
-    </template>
+    
+</template>
 <script setup>  
-import { ref } from 'vue';
-const dialog = ref(false);
-const loaded = ref(false)
-const loading = ref(false)
 
-function onClick () {
-    loading.value = true
-    setTimeout(() => {
-    loading.value = false
-    loaded.value = true
-    }, 2000)
-}
 </script>
