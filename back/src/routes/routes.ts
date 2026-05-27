bf24a3dceaad335c96ec7b0b9c5824fc3ef861cd
VitoriaSant <svitoria3465@gmail.com>
2026-05-26 21:16:25 -0300

    feat: novas rotas de pedido de compra pendente criadas

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,7 +1,6 @@
 //Express
 import { Router } from 'express';
 //Controllers
-import ComprasPendentesController from '../controllers/pedidos-compra-pendente/PedidosCompraPendentesController';
 import Teste from '../controllers/teste.controlles';
 import PontoDeCompraController from '../controllers/ponto-de-compra/PontoDeCompraController';
 import ResumoPedidoCompraPendenteController from '../controllers/pedidos-compra-pendente/resumo/ResumoPedidoCompraPendenteController';
@@ -10,7 +9,6 @@ import ItensCompraPendenteController from '../controllers/pedidos-compra-pendent
 import FornecedoresPedidoCompraPendenteController from '../controllers/pedidos-compra-pendente/fornecedores-pendentes/FornecedoresPedidoCompraPendenteController';
 
 const router = Router();
-const comprasPendentesController = new ComprasPendentesController();
 const pontoDeCompraController = new PontoDeCompraController();
 const teste = new Teste();
 const resumoPedidoCompraPendenteController = new ResumoPedidoCompraPendenteController();
@@ -19,14 +17,13 @@ const itensCompraPendenteController = new ItensCompraPendenteController();
 const fornecedoresPedidoCompraPendenteController = new FornecedoresPedidoCompraPendenteController();
 
 router.get('/', teste.get);
-router.post('/compraPendente', comprasPendentesController.pedidoCompraPendente);
 router.post('/pontoDeCompra', pontoDeCompraController.pontoDeCompra);
 
 //Pedido de compra pendete
-router.post('/resumoPedidoCompraPendente', resumoPedidoCompraPendenteController.resumoPedidoCompraPendente);
-router.get('/pedidoCompraPendente', pedidoCompraPendenteController.PedidoCompraPendente);
-router.get('/itensCompraPendente', itensCompraPendenteController.ItensCompraPendente);
-router.get(
+router.post('/resumoPedidoCompraPendente', resumoPedidoCompraPendenteController.ResumoPedidoCompraPendente);
+router.post('/pedidoCompraPendente', pedidoCompraPendenteController.PedidoCompraPendente);
+router.post('/itensCompraPendente', itensCompraPendenteController.ItensCompraPendente);
+router.post(
   '/fornecedoresPedidoCompraPendente',
   fornecedoresPedidoCompraPendenteController.FornecedoresPedidoCompraPendente,
 );
