7410a29b44de973b92a8a508628931b5aabdd5fc
VitoriaSant <svitoria3465@gmail.com>
2026-05-27 00:26:49 -0300

    feat: criacao das funcoes no Store para a busca dos dados das rotas de fornecedor, itens, resumo e pedidos

--- front/src/stores/PedidoPendenteStore.ts
+++ front/src/stores/PedidoPendenteStore.ts
@@ -2,21 +2,51 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 //Services
-import PedidoCompraPendenteController from '@/Service/tema-estoque/pedidos-compra-pendente/PedidoCompraPendenteController';
+
+import ResumoPedidoCompraPendenteController from '@/Service/tema-estoque/pedidos-compra-pendente/resumo-pedido-compra-pendente/ResumoPedidoCompraPendeteController';
+import FornecedorPedidoCompraPendenteController from '@/Service/tema-estoque/pedidos-compra-pendente/fornecedores-pedido-compra-pendente/FornecedoresPedidoCompraPendenteController';
+import ItensCompraPendenteController from '@/Service/tema-estoque/pedidos-compra-pendente/itens-compra-pendente/ItensCompraPendenteController';
+import PedidoCompraPendenteController from '@/Service/tema-estoque/pedidos-compra-pendente/pedido-compra-pendente/PedidoCompraPendenteController';
+
 import CClasseFiltro from '@/Service/base/CClasseFiltro';
 //Interfaces
-import type { IPedidoCompraPendenteResponse } from '@/Service/tema-estoque/pedidos-compra-pendente/IPedidoCompraPendenteResonse';
+import type { IResumopedidoCompraPendete } from '@/Service/tema-estoque/pedidos-compra-pendente/resumo-pedido-compra-pendente/IResumoPedidoCompraPendete';
+import type { IFornecedoresPedidoCompraPendente } from '@/Service/tema-estoque/pedidos-compra-pendente/fornecedores-pedido-compra-pendente/IFornecedoresPedidoCompraPendente';
+import type { IItensCompraPendente } from '@/Service/tema-estoque/pedidos-compra-pendente/itens-compra-pendente/IItensCompraPendente';
+import type { IPedidoCompraPendente } from '@/Service/tema-estoque/pedidos-compra-pendente/pedido-compra-pendente/IPedidoCompraPendente';
+
 //Models
 import type CPedidoCompraPendenteModel from '@/Service/tema-estoque/pedidos-compra-pendente/CPedidoCompraPendenteModel';
 
 export const usePedidoPendenteStore = defineStore('[PedidoPendenteStore]', () => {
-  const controller = new PedidoCompraPendenteController();
-  const filtrarComprasPendentes = ref(
-    async (classeFiltro: CClasseFiltro<CPedidoCompraPendenteModel>): Promise<IPedidoCompraPendenteResponse> =>
-      controller.listarComprasPendentes(classeFiltro),
+  const resumoPedidoCompraPendenteController = new ResumoPedidoCompraPendenteController();
+  const filtrarResumoPedidoCompraPendete = ref(
+    async (classeFiltro: CClasseFiltro<CPedidoCompraPendenteModel>): Promise<IResumopedidoCompraPendete> =>
+      resumoPedidoCompraPendenteController.resumoPedidoCompraPendete(classeFiltro),
+  );
+
+  const fornecedorPedidoCompraPendenteController = new FornecedorPedidoCompraPendenteController();
+  const filtrarFornecedorPedidoCompraPendente = ref(
+    async (classeFiltro: CClasseFiltro<CPedidoCompraPendenteModel>): Promise<IFornecedoresPedidoCompraPendente> =>
+      fornecedorPedidoCompraPendenteController.fornecedorPedidoCompraPendete(classeFiltro),
+  );
+
+  const itensCompraPendenteController = new ItensCompraPendenteController();
+  const filtrarItensCompraPendente = ref(
+    async (classeFiltro: CClasseFiltro<CPedidoCompraPendenteModel>): Promise<IItensCompraPendente> =>
+      itensCompraPendenteController.itensPedidoCompraPendete(classeFiltro),
+  );
+
+  const pedidoCompraPendenteController = new PedidoCompraPendenteController();
+  const filtrarPedidoCompraPendente = ref(
+    async (classeFiltro: CClasseFiltro<CPedidoCompraPendenteModel>): Promise<IPedidoCompraPendente> =>
+      pedidoCompraPendenteController.pedidoCompraPendete(classeFiltro),
   );
 
   return {
-    filtrarComprasPendentes,
+    filtrarResumoPedidoCompraPendete,
+    filtrarFornecedorPedidoCompraPendente,
+    filtrarItensCompraPendente,
+    filtrarPedidoCompraPendente,
   };
 });
