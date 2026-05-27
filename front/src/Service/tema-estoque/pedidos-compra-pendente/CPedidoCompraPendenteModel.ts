de0f1f422d208909c469d0d40b56ce6efb4c4e5d
VitoriaSant <svitoria3465@gmail.com>
2026-05-27 00:28:44 -0300

    delete: exclusao dos filtros que nao serao utilizados

--- front/src/Service/tema-estoque/pedidos-compra-pendente/CPedidoCompraPendenteModel.ts
+++ front/src/Service/tema-estoque/pedidos-compra-pendente/CPedidoCompraPendenteModel.ts
@@ -1,8 +1,4 @@
 export default class CPedidoCompraPendenteModel {
-  //codigo: number = 0;
-  //empresaId: number = 0;
-  //dataEmissao: Date | null = null;
-  //dataPrevisaoEntrega: Date | null = null;
   fornecedorId: number = 0;
   razaoSocialFornecedor: string = '';
   itemId: number = 0;
@@ -13,8 +9,6 @@ export default class CPedidoCompraPendenteModel {
   descricaoCor: string = '';
   acabamentoId: number = 0;
   descricaoAcabamento: string = '';
-  //quantidadeAberta: number = 0;
-  //valorUnitarioLiquido: number = 0;
 
   private constructor(data: Partial<CPedidoCompraPendenteModel>) {
     Object.assign(this, data);
