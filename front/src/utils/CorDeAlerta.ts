95adf6901d66872613af59bf663b18e9b804cd8e
VitoriaSant <svitoria3465@gmail.com>
2026-05-29 00:09:11 -0300

    feat: criacao do uma funcao de cor de alerta para ser utilizada quando houver a necessidade de mudar a cor da linha na tabela

---
+++ front/src/utils/CorDeAlerta.ts
@@ -0,0 +1,16 @@
+export default class CorDeAlerta {
+  public static alerta(pData: Date | undefined | null): string {
+    if (pData === undefined || pData === null) return '';
+
+    const diaAtual = new Date();
+
+    const tempoData = new Date(pData).getTime();
+    const tempoAtual = diaAtual.getTime();
+
+    if (tempoData < tempoAtual) {
+      return 'vermelho';
+    }
+
+    return 'normal';
+  }
+}
